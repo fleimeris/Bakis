117a0e5a0b86397addc82945c6921264d387d9c2
fleimeris <44847763+fleimeris@users.noreply.github.com>
2023-05-01 15:48:32 +0300

    add list of rules

--- Frontend/Pages/AuditRules.razor.cs
+++ Frontend/Pages/AuditRules.razor.cs
@@ -1,14 +1,22 @@
+using System.Net.Http.Headers;
 using Frontend.Components;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
+using Newtonsoft.Json;
 
 namespace Frontend.Pages;
 
 public partial class AuditRules
 {
     [Inject] private IDialogService _dialogService { get; set; }
-    private readonly List<AuditRules> _auditRules = new();
-    
+    [Inject] private ISnackbar _snackbar { get; set; }
+    private List<RestAPI.Domain.Data.Models.AuditRule> _auditRules = new();
+
+    protected override async Task OnInitializedAsync()
+    {
+        await RefreshRules();
+    }
+
     private async Task OnCreateButton()
     {
         var result = await _dialogService.ShowAsync<CreateAuditRule>("Create new audit rule");
@@ -17,7 +25,37 @@ public partial class AuditRules
 
         if ((bool)isSubmitted.Data)
         {
+            await RefreshRules();
+        }
+    }
+
+    private async Task RefreshRules()
+    {
+        var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        httpClient.Timeout = TimeSpan.FromHours(1);
+
+        var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:5254/api/v1/AuditRule");
+
+        try
+        {
+            var result = await httpClient.SendAsync(request);
+
+            if (!result.IsSuccessStatusCode)
+            {
+                _snackbar.Add("Failed to receive all audit rules", Severity.Error);
+                return;
+            }
+
+            var responseString = await result.Content.ReadAsStringAsync();
             
+            _auditRules.Clear();
+            _auditRules = JsonConvert.DeserializeObject<List<RestAPI.Domain.Data.Models.AuditRule>>(responseString);
+            await InvokeAsync(StateHasChanged);
+        }
+        catch (Exception e)
+        {
+            _snackbar.Add($"Failed to receive all audit rules: {e}", Severity.Error);
         }
     }
 }
