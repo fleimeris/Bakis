5598ce171516e521e4e27ae413bf99f0fb97da6c
fleimeris <44847763+fleimeris@users.noreply.github.com>
2023-05-20 14:56:16 +0300

    Save to file

--- RestAPI.Domain/Services/RuleService/RuleService.cs
+++ RestAPI.Domain/Services/RuleService/RuleService.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using RestAPI.Domain.Data.Enums;
 using RestAPI.Domain.Data.Models;
 
@@ -5,8 +6,6 @@ namespace RestAPI.Domain.Services.RuleService;
 
 public class RuleService : IRuleService
 {
-    private readonly List<AuditRule> _data = new();
-
     public Guid Insert(string identifier, CookieCategory category)
     {
         var id = Guid.NewGuid();
@@ -16,37 +15,51 @@ public class RuleService : IRuleService
             Identifier = identifier,
             Category = category
         };
+
+        var rules = GetAll();
+        rules.Add(data);
         
-        _data.Add(data);
+        File.WriteAllText("db.json", JsonConvert.SerializeObject(rules));
 
         return id;
     }
 
     public void Delete(Guid ruleId)
     {
-        var rule = _data.FirstOrDefault(x => x.Id == ruleId);
+        var rules = GetAll();
+        
+        var rule = rules.FirstOrDefault(x => x.Id == ruleId);
 
         if (rule != null)
-            _data.Remove(rule);
+            rules.Remove(rule);
+        
+        File.WriteAllText("db.json", JsonConvert.SerializeObject(rules));
     }
 
     public AuditRule? GetById(Guid ruleId)
     {
-        return _data.FirstOrDefault(x => x.Id == ruleId);
+        var rules = GetAll();
+        
+        return rules.FirstOrDefault(x => x.Id == ruleId);
     }
 
     public List<AuditRule> GetAll()
     {
-        return _data;
+        var fileData = File.ReadAllText("db.json");
+        var result = JsonConvert.DeserializeObject<List<AuditRule>>(fileData);
+
+        return result ?? new List<AuditRule>();
     }
-    
+
     public void Update(Guid ruleId, string identifier, CookieCategory category)
     {
-        var value = _data.FirstOrDefault(x => x.Id == ruleId);
+        var rules = GetAll();
         
-        if(value == null)
+        var value = rules.FirstOrDefault(x => x.Id == ruleId);
+
+        if (value == null)
             return;
-        
+
         value.Identifier = identifier;
         value.Category = category;
     }
